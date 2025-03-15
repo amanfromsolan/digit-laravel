22bd3b409284e626e569d59b0b2012c22227a94b
Aman <aman@hey.com>
2025-03-15 13:14:43 +0530

    Add SocialLogin model with User relationship and refactor User model

---
+++ app/Models/SocialLogin.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class SocialLogin extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        "user_id",
+        "provider",
+        "provider_id",
+        "provider_token",
+        "provider_data",
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        "provider_data" => "array",
+    ];
+
+    /**
+     * Get the user that owns this social login.
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
